ab54256c59f6f34c39325a4bb11f409bc94a295d
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-07 18:16:04 +0900

    [add]address automatic_input function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :prefecture_code, :city, :street])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     #sign_upの際にnameのデータ操作を許。追加したカラム。
   end
 end
